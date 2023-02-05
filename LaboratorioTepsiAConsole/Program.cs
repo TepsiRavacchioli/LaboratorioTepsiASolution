97af19af1e76f1baaeeb08379293f8415e302a67
RavacchioliGit <cristina.ravacchioli@gmail.com>
2023-02-05 12:31:45 +0100

    Conversione di un numero decimale inserito da utente in una base scelta da utente [2,16]

--- LaboratorioTepsiAConsole/Program.cs
+++ LaboratorioTepsiAConsole/Program.cs
@@ -10,13 +10,47 @@ namespace LaboratorioTepsiAConsole
             string res = "";
             Console.WriteLine("ins num dec");
             num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("quale base vuoi? implementate fino a 16 ");
+            int response = Convert.ToInt32(Console.ReadLine());
+            int baseConv = response;
+
+
             while (num > 0)
             {
-                int resto = num % 2;
-                res = resto + res;
-                num = num / 2;
+                int resto = num % baseConv;
+
+                string alfa = "";
+                switch (resto)
+                {
+                    case 10:
+                        alfa = "A";
+                        break;
+                    case 11:
+                        alfa = "B";
+                        break;
+                    case 12:
+                        alfa = "C";
+                        break;
+                    case 13:
+                        alfa = "D";
+                        break;
+                    case 14:
+                        alfa = "E";
+                        break;
+                    case 15:
+                        alfa = "F";
+                        break;
+                    default:
+                        alfa = alfa + resto;
+                        break;
+                }
+                res = alfa + res;
+                num = num / baseConv;
             }
+
+            Console.WriteLine($" la base che ho scelto è {baseConv}");
             Console.WriteLine($" risultato {res}");
+            Console.WriteLine($" num attuale {num}");
         }
     }
 }
