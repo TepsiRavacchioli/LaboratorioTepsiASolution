9ea46c63a6dd9d336bfd2aebc85510a408e67dc2
RavacchioliGit <cristina.ravacchioli@gmail.com>
2023-02-05 12:24:57 +0100

    Add project files.

---
+++ LaboratorioTepsiAConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LaboratorioTepsiAConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
